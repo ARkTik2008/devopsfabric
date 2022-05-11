a8beaf102be1c31cf773febddcc74da544d52894
Dmitriy Pashkov <pashkov.dmitriy@gmail.com>
2022-05-11 09:30:59 +0300

    DEVOPS-008 add fw_reset.sh and countdown.sh

---
+++ DEVOPS-008/fw_reset.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# This script is used to securely return the firewall to default settings.
+# Running in a tmux session, it allows to securely change the firewall rules.
+# The script waits 10 minutes from its start and disables the firewall,
+# after that it waits another 10 minutes and restarts the server. In total, you
+# have 20 minutes before restarting the server.
+#
+# If the countdown timer script (countdown.sh) is placed near this script,
+# the remaining minutes will be displayed.
+#
+# Usage: sudo ./fw_reset.sh
+#
+# Attach a running session: sudo tmux attach -t fw_reset
+# Detach a running session: sudo tmux detach
+# Stop and exit a session: sudo tmux kill-session -t fw_reset
+#
+
+set -e
+
+tmux new-session -d -s fw_reset
+tmux split-window -v
+tmux select-pane -U
+tmux send-keys -t fw_reset 'bash ./countdown.sh' C-m
+tmux select-pane -D
+tmux send-keys -t fw_reset "sleep 600 \
+  && echo '$(date) disable firewall' && sleep 600 && reboot" C-m
+#tmux attach-session -d -t fw_reset

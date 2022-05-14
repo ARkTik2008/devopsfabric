db30855f6f27b6063ba150c160afb35d3e958ee1
Dmitriy Pashkov <pashkov.dmitriy@gmail.com>
2022-05-14 22:09:27 +0300

    DEVOPS-011 extra nginx configuration

---
+++ DEVOPS-011/nginx_supersecret.fabric.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# nginx_supersecret.fabric.sh
+#
+# This script add new virtual server 'supersecret.fabric' that:
+#  - accepts requests on port 90/TCP,
+#  - uses HTTP Basic Authentication,
+#  - uses 'location /proxy' for proxy requests to daemon
+#       `python3 -m http.server 8000`, running in the tmux,
+#  - writes access.log in JSON format.
+#
+# Usage: sudo ./nginx_supersecret.fabric.sh
+#
+
+set -e
+
+# check for root
+if [[ "$(id -u)" != "0" ]];then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
+# adding new virtual server config and json logging config
+cp --force {supersecret.fabric.conf,logging.conf} /etc/nginx/conf.d/
+
+# checking && reloading NGINX config
+nginx -t && nginx -s reload
+
+# adding directiry for new virtual server
+mkdir -p /var/www/supersecret.fabric/html
+
+# copiing index.html and hello.html
+cp --force {index.html,hello.html} /var/www/supersecret.fabric/html/
+
+# copiing auth.basic file
+cp --force auth.basic /etc/nginx/
+
+# attaching or starting tmux session with python3 webserver
+mkdir -p /var/www/supersecret.fabric/html/proxy/
+cp --force hello.html /var/www/supersecret.fabric/html/proxy/
+tmux attach -t python-server \
+  || tmux new-session -d -s python-server "python3 -m http.server 8000" \
+  && tmux attach -t python-server
